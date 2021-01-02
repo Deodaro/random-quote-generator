c31756483b47b6b4d6c5b9a5f07059cf39447f30
Deodaro <l_r@list.ru>
2021-01-02 21:36:33 +1000

    Create localQuotes.js

---
+++ localQuotes.js
@@ -0,0 +1,198 @@
+const localQuotes = [
+  {
+    "text": "Act as if what you do makes a difference. It does.",
+    "author": "William James"
+  },
+  {
+    "text": "Passion creates the desire for more and action fuelled by passion creates a future.",
+    "author": "Byron Pulsifer"
+  },
+  {
+    "text": "Do good by stealth, and blush to find it fame.",
+    "author": "Alexander Pope"
+  },
+  {
+    "text": "Opportunity often comes disguised in the form of misfortune, or temporary defeat.",
+    "author": "Napoleon Hill"
+  },
+  {
+    "text": "Don't talk about what you have done or what you are going to do.",
+    "author": "Thomas Jefferson"
+  },
+  {
+    "text": "Most powerful is he who has himself in his own power.",
+    "author": "Seneca"
+  },
+  {
+    "text": "We don't stop playing because we grow old; we grow old because we stop playing.",
+    "author": "Bernard Shaw"
+  },
+  {
+    "text": "Experience can only be gained by doing not by thinking or dreaming.",
+    "author": "Byron Pulsifer"
+  },
+  {
+    "text": "Always tell the truth. That way, you don't have to remember what you said.",
+    "author": "Mark Twain"
+  },
+  {
+    "text": "From wonder into wonder existence opens.",
+    "author": "Lao Tzu"
+  },
+  {
+    "text": "He who fears being conquered is sure of defeat.",
+    "author": "Napoleon Bonaparte"
+  },
+  {
+    "text": "Life is what happens while you are making other plans.",
+    "author": "John Lennon"
+  },
+  {
+    "text": "Doing what you love is the cornerstone of having abundance in your life.",
+    "author": "Wayne Dyer"
+  },
+  {
+    "text": "Kindness is the golden chain by which society is bound together.",
+    "author": "Johann Wolfgang von Goethe"
+  },
+  {
+    "text": "You need chaos in your soul to give birth to a dancing star.",
+    "author": "Nietzsche"
+  },
+  {
+    "text": "It can't be spring if your heart is filled with past failures.",
+    "author": "Byron Pulsifer"
+  },
+  {
+    "text": "No yesterdays are ever wasted for those who give themselves to today.",
+    "author": "Brendan Francis"
+  },
+  {
+    "text": "There are no failures just experiences and your reactions to them.",
+    "author": "Tom Krause"
+  },
+  {
+    "text": "Action is the foundational key to all success.",
+    "author": "Pablo Picasso"
+  },
+  {
+    "text": "What is necessary to change a person is to change his awareness of himself.",
+    "author": "Abraham Maslow"
+  },
+  {
+    "text": "Positive thinking will let you do everything better than negative thinking will.",
+    "author": "Zig Ziglar"
+  },
+  {
+    "text": "We shall never know all the good that a simple smile can do.",
+    "author": "Mother Teresa"
+  },
+  {
+    "text": "Nothing is so strong as gentleness. Nothing is so gentle as real strength.",
+    "author": "Frances de Sales"
+  },
+  {
+    "text": "Imagination is not a talent of some men but is the health of every man.",
+    "author": "Ralph Waldo Emerson"
+  },
+  {
+    "text": "We must embrace pain and burn it as fuel for our journey.",
+    "author": "Kenji Miyazawa"
+  },
+  {
+    "text": "Don't wait for people to be friendly. Show them how.",
+    "author": null
+  },
+  {
+    "text": "A gem cannot be polished without friction, nor a man perfected without trials.",
+    "author": "Chinese proverb"
+  },
+  {
+    "text": "Each day can be one of triumph if you keep up your interests.",
+    "author": "George Matthew Adams"
+  },
+  {
+    "text": "The place to improve the world is first in one's own heart and head and hands.",
+    "author": "Robert M. Pirsig"
+  },
+  {
+    "text": "The pessimist sees difficulty in every opportunity. The optimist sees the opportunity in every difficulty.",
+    "author": "Winston Churchill"
+  },
+  {
+    "text": "Winners have simply formed the habit of doing things losers don't like to do.",
+    "author": "Albert Gray"
+  },
+  {
+    "text": "Nature is a mutable cloud which is always and never the same.",
+    "author": "Ralph Emerson"
+  },
+  {
+    "text": "Life is what you make of it. Always has been, always will be.",
+    "author": "Grandma Moses"
+  },
+  {
+    "text": "Worry often gives a small thing a big shadow.",
+    "author": "Swedish proverb"
+  },
+  {
+    "text": "I want you to be everything that's you, deep at the center of your being.",
+    "author": "Confucius"
+  },
+  {
+    "text": "We know what we are, but know not what we may be.",
+    "author": "William Shakespeare"
+  },
+  {
+    "text": "Freedom is what you do with what's been done to you.",
+    "author": "Jean-Paul Sartre"
+  },
+  {
+    "text": "The truth which has made us free will in the end make us glad also.",
+    "author": "Felix Adler"
+  },
+  {
+    "text": "He who has imagination without learning has wings but no feet.",
+    "author": "Joseph Joubert"
+  },
+  {
+    "text": "Whoso loves, believes the impossible.",
+    "author": "Elizabeth Browning"
+  },
+  {
+    "text": "It isn't where you come from, it's where you're going that counts.",
+    "author": "Ella Fitzgerald"
+  },
+  {
+    "text": "The greatest obstacle to connecting with our joy is resentment.",
+    "author": "Pema Chodron"
+  },
+  {
+    "text": "When anger use your energy to do something productive.",
+    "author": "C. Pulsifer"
+  },
+  {
+    "text": "We are all something, but none of us are everything.",
+    "author": "Blaise Pascal"
+  },
+  {
+    "text": "If you can't explain it simply, you don't understand it well enough.",
+    "author": "Albert Einstein"
+  },
+  {
+    "text": "He who lives in harmony with himself lives in harmony with the world.",
+    "author": "Marcus Aurelius"
+  },
+  {
+    "text": "He who knows himself is enlightened.",
+    "author": "Lao Tzu"
+  },
+  {
+    "text": "Build a better mousetrap and the world will beat a path to your door.",
+    "author": "Ralph Emerson"
+  },
+  {
+    "text": "As our case is new, we must think and act anew.",
+    "author": "Abraham Lincoln"
+  }
+];
